940a2b47f8899db16c0db98c8557d0caaf5eea4f
kikito <kikito@gmail.com>
2010-03-23 23:18:32 +0000

    Updated MiddleClass in order to comply with naming conventions. Fixed several bugs on MindState

--- MiddleClass.lua
+++ MiddleClass.lua
@@ -4,114 +4,118 @@
 -- Based on YaciCode, from Julien Patte and LuaObject, from Sébastien Rocca-Serra
 -----------------------------------------------------------------------------------
 
-local classes = setmetatable({}, {__mode = "k"})   -- weak table storing references to all declared classes
+local _classes = setmetatable({}, {__mode = "k"})   -- weak table storing references to all declared _classes
 
 -- The 'Object' class
 Object = { name = "Object" }
 
-  -- creates a new instance
-Object.new = function(class, ...)
-  assert(classes[class]~=nil, "Use class:new instead of class.new")
+_classes[Object]=Object -- adds Object to the list of _classes
 
-  local instance = setmetatable({ class = class }, class.__classDict) -- the class dictionary is the instance's metatable
+  -- creates a new instance
+Object.new = function(theClass, ...)
+  assert(_classes[theClass]~=nil, "Use class:new instead of class.new")
+
+  local instance = setmetatable({ class = theClass }, theClass.__classDict) -- the class dictionary is the instance's metatable
   instance:initialize(...)
   return instance
 end
 
 -- creates a subclass
-Object.subclass = function(superclass, name)
-  assert(classes[superclass]~=nil, "Use class:subclass instead of class.subclass")
+Object.subclass = function(theClass, name)
+  assert(_classes[theClass]~=nil, "Use class:subclass instead of class.subclass")
   if type(name)~="string" then name = "Unnamed" end
 
-  local theClass = { name = name, superclass = superclass, __classDict = {} }
-  local classDict = theClass.__classDict
+  local theSubclass = { name = name, superclass = theClass, __classDict = {} }
+  local classDict = theSubclass.__classDict
 
   -- This one is weird. Since:
   -- a) the class dict is the instances' metatable (so it must have an __index for looking up the methods) and
   -- b) The instance methods are in the class dict itself, then ...
   classDict.__index = classDict
   -- if a method isn't found on the class dict, look on its super class
-  setmetatable(classDict, {__index = superclass.__classDict} )
-  -- theClass also needs some metamethods
-  setmetatable(theClass, {
+  setmetatable(classDict, {__index = theClass.__classDict} )
+
+  setmetatable(theSubclass, {   -- theSubclass' metamethods
     __index = function(_,methodName)
-      local localMethod = classDict[methodName] -- this allows using classDic as a class method AND instance method dict
-      if localMethod ~= nil then return localMethod end
-      return superclass[methodName]
-    end,
+        local localMethod = classDict[methodName] -- this allows using classDic as a class method AND instance method dict
+        if localMethod ~= nil then return localMethod end
+        return theClass[methodName]
+      end,
     -- FIXME add support for __index method here
     __newindex = function(_, methodName, method) -- when adding new methods, include a "super" function
-      if type(method) == 'function' then
-        local fenv = getfenv(method)
-        local newenv = setmetatable( {super = superclass.__classDict},  {__index = fenv, __newindex = fenv} )
-        setfenv( method, newenv )
-      end
-      rawset(classDict, methodName, method)
-    end,
+        if type(method) == 'function' then
+          local fenv = getfenv(method)
+          local newenv = setmetatable( {super = theClass.__classDict},  {__index = fenv, __newindex = fenv} )
+          setfenv( method, newenv )
+        end
+        rawset(classDict, methodName, method)
+      end,
     __tostring = function() return ("class ".. name) end,
-    __call = function(_, ...) return theClass:new(...) end
+    __call = function(_, ...) return theSubclass:new(...) end
   })
   -- instance methods go after the setmetatable, so we can use "super"
-  theClass.initialize = function(instance,...) super.initialize(instance) end
+  theSubclass.initialize = function(instance,...) super.initialize(instance) end
 
-  classes[theClass]=theClass --registers the new class on the list of classes
+  _classes[theSubclass]=theSubclass --registers the new class on the list of _classes
 
-  return theClass
+  return theSubclass
 end
 
-  -- Mixin extension function - simulates very basically ruby's include(module)
-  -- module is a lua table of functions. The functions will be copied to the class
-  -- if present in the module, the included() method will be called
-Object.includes = function(class, module, ... )
-  assert(classes[class]~=nil, "Use class:includes instead of class.includes")
+-- Mixin extension function - simulates very basically ruby's include(module)
+-- module is a lua table of functions. The functions will be copied to the class
+-- if present in the module, the included() method will be called
+Object.includes = function(theClass, module, ... )
+  assert(_classes[theClass]~=nil, "Use class:includes instead of class.includes")
   for methodName,method in pairs(module) do
-    if methodName ~="included" then class[methodName] = method end
+    if methodName ~="included" then theClass[methodName] = method end
   end
-  if type(module.included)=="function" then module:included(class, ... ) end
+  if type(module.included)=="function" then module:included(theClass, ... ) end
 end
 
-
-classes[Object]=Object -- adds Object to the list of classes
-
+-- root of initialize and __tostring methods
 Object.__classDict = {
   initialize = function(instance, ...) end,   -- end of the initialize() call chain
   __tostring = function(instance) return ("instance of ".. instance.class.name) end
 }
 
+-- This allows doing tostring(obj) and Object() instead of Object:new()
 setmetatable(Object, { __index = Object.__classDict, __newindex = Object.__classDict,
   __tostring = function() return ("class Object") end,
   __call = Object.new
 })
 
-function Object.getterFor(class, attr) return 'get' .. attr:gsub("^%l", string.upper) end
-function Object.setterFor(class, attr) return 'set' .. attr:gsub("^%l", string.upper) end
-function Object.getter(class, attributeName, defaultValue)
-  class[class:getterFor(attributeName)] = function(self) 
+-- Getter/Setter related methods
+function Object.getterFor(theClass, attr) return 'get' .. attr:gsub("^%l", string.upper) end
+function Object.setterFor(theClass, attr) return 'set' .. attr:gsub("^%l", string.upper) end
+function Object.getter(theClass, attributeName, defaultValue)
+  theClass[theClass:getterFor(attributeName)] = function(self) 
     if(self[attributeName]~=nil) then return self[attributeName] end
     return defaultValue
   end
 end
-function Object.setter(class, attributeName)
-  class[class:setterFor(attributeName)] = function(self, value) self[attributeName] = value end
+function Object.setter(theClass, attributeName)
+  theClass[theClass:setterFor(attributeName)] = function(self, value) self[attributeName] = value end
 end
-function Object.getterSetter(class, attributeName, defaultValue)
-  class:getter(attributeName, defaultValue)
-  class:setter(attributeName)
-end
-
--- Returns true if class is a subclass of other, false otherwise
-function subclassOf(other, class)
-  if class.superclass==nil then return false end --class is Object, or a non-class
-  return class.superclass == other or subclassOf(other, class.superclass)
+function Object.getterSetter(theClass, attributeName, defaultValue)
+  theClass:getter(attributeName, defaultValue)
+  theClass:setter(attributeName)
 end
 
--- Returns true if obj is an instance of class (or one of its subclasses) false otherwise
-function instanceOf(class, obj)
-  if obj==nil or classes[class]==nil or classes[obj.class]==nil then return false end
-  if obj.class==class then return true end
-  return subclassOf(class, obj.class)
+-- Returns true if aClass is a subclass of other, false otherwise
+function subclassOf(other, aClass)
+  if aClass == nil or other==nil then return false end
+  if aClass.superclass==nil then return false end -- aClass is Object, or a non-class
+  return aClass.superclass == other or subclassOf(other, aClass.superclass)
 end
 
+-- Returns true if obj is an instance of aClass (or one of its subclasses) false otherwise
+function instanceOf(aClass, obj)
+  if obj==nil or _classes[aClass]==nil or _classes[obj.class]==nil then return false end
+  if obj.class==aClass then return true end
+  return subclassOf(aClass, obj.class)
+end
+
+-- Creates a new class named 'name'. It uses baseClass as the parent (Object if none specified)
 function class(name, baseClass)
   baseClass = baseClass or Object
   return baseClass:subclass(name)
--- MindState.lua
+++ MindState.lua
@@ -4,7 +4,8 @@
 -- Based on Unrealscript's stateful objects
 -----------------------------------------------------------------------------------
 
-require 'MiddleClass.lua'
+assert(Object~=nil and class~=nil, 'MiddleClass not detected. Please require it before using MindState')
+
 --[[ StatefulObject declaration
   * Stateful classes have a list of states (accesible through class.states).
   * When a method is invoked on an instance of such classes, it is first looked up on the class current state (accesible through class.currentState)
@@ -15,9 +16,37 @@ StatefulObject = class('StatefulObject')
 
 StatefulObject.states = {} -- the root state list
 
-local private = setmetatable({}, {__mode = "k"})   -- weak table storing private references
+------------------------------------
+-- PRIVATE ATTRIBUTES AND METHODS
+------------------------------------
+local _private = setmetatable({}, {__mode = "k"})   -- weak table storing private references
 
--- Instance methods
+-- helper function used to call state callbacks (enterState, exitState, etc)
+local _invokeCallback = function(self, state, callbackName, ... )
+  if state==nil then return end
+  local callback = state[callbackName]
+  if(type(callback)=='function') then callback(self, ...) end
+end
+
+local _getStack=function(self)
+  local stack = _private[self].stateStack
+  assert(stack~=nil, "Could not find the stack for the object. Make sure you invoked super.initialize(self) on the constructor.")
+  return stack
+end
+
+-- These methods will not be overriden by the states.
+local _ignoredMethods = {
+  states=1, initialize=1,
+  gotoState=1, pushState=1, popState=1, popAllStates=1, getCurrentState=1, isInState=1,
+  enterState=1, exitState=1, pushedState=1, poppedState=1, pausedState=1, continuedState=1,
+  addState=1, subclass=1, includes=1
+}
+
+local _prevSubclass = StatefulObject.subclass -- previous way of creating subclasses (used to redefine subclass itself)
+
+------------------------------------
+-- INSTANCE METHODS
+------------------------------------
 
 --[[ constructor
   If your states need initialization, they can receive parameters via the initParameters parameter
@@ -27,14 +56,16 @@ local private = setmetatable({}, {__mode = "k"})   -- weak table storing private
 function StatefulObject:initialize(initParameters)
   super.initialize(self)
   initParameters = initParameters or {} --initialize to empty table if nil
-  self.states = {}
-  private[self] = {
+
+  _private[self] = {
+    states = {},
     stateStack = {}
   }
+
   for stateName,stateClass in pairs(self.class.states) do 
     local state = stateClass:new(unpack(initParameters[stateName] or {}))
     state.name = stateName
-    self.states[stateName] = state
+    _private[self].states[stateName] = state
   end
 end
 
@@ -46,7 +77,7 @@ end
   Second parameter is optional. If true, the stack will be conserved. Otherwise, it will be popped.
 ]]
 function StatefulObject:gotoState(newStateName, keepStack)
-  assert(self.states~=nil, "Attribute 'states' not detected. check that you called instance:gotoState and not instance.gotoState, and that you invoked super.initialize(self) in the constructor.")
+  assert(_private[self].states~=nil, "Attribute 'states' not detected. check that you called instance:gotoState and not instance.gotoState, and that you invoked super.initialize(self) in the constructor.")
 
   local prevState = self:getCurrentState()
 
@@ -55,80 +86,95 @@ function StatefulObject:gotoState(newStateName, keepStack)
 
   local nextState
   if(newStateName~=nil) then
-    nextState = self.states[newStateName]
+    nextState = _private[self].states[newStateName]
     assert(nextState~=nil, "State '" .. newStateName .. "' not found")
   end
 
-  -- Invoke exitState on the previous state
-  if(prevState~=nil and type(prevState.exitState) == "function") then prevState.exitState(self, newStateName) end
-
-  -- Empty the stack unless keepStack is true.
-  if(keepStack~=true) then self:popAllStates() end
+  -- Either empty completely the stack, or just call the exitstate callback on current state
+  if(keepStack~=true) then 
+    self:popAllStates()
+  else
+    _invokeCallback(self, prevState, 'exitState', newStateName )
+  end
 
   -- replace the top of the stack with the new state
-  local stack = private[self].stateStack
+  local stack = _getStack(self)
   stack[math.max(#stack,1)] = nextState
 
   -- Invoke enterState on the new state. 2nd parameter is the name of the previous state, or nil
-  if(nextState~=nil and type(nextState.enterState) == "function") then
-    nextState.enterState(self, prevState~=nil and prevState.name or nil)
-  end
+  _invokeCallback(self, nextState, 'enterState', prevState~=nil and prevState.name or nil)
+
 end
 
+--[[ Changes the current state, by pushing a new state on the stack.
+  If the pushed state is already on the stack, this function does nothing.
+  Invokes 'pausedState' on the previous state, if existing
+  The new state is pushed on the top of the stack and then
+  Invokes 'pushedState' and 'enterState' on the new state, if existing
+]]
 function StatefulObject:pushState(newStateName)
-  assert(type(newState)=='string', "newStateName must be a string.")
-  assert(self.states~=nil, "Attribute 'states' not detected. check that you called instance:pushState and not instance.pushState, and that you invoked super.initialize(self) in the constructor.")
+  assert(type(newStateName)=='string', "newStateName must be a string.")
+  assert(_private[self].states~=nil, "Attribute 'states' not detected. check that you called instance:pushState and not instance.pushState, and that you invoked super.initialize(self) in the constructor.")
 
-  local nextState = self.states[newStateName]
+  local nextState = _private[self].states[newStateName]
   assert(nextState~=nil, "State '" .. newStateName .. "' not found")
 
   -- If we attempt to push a state and the state is already on return (do nothing)
-  local stack = private[self].stateStack
+  local stack = _getStack(self)
   for _,state in ipairs(stack) do 
     if(state.name == newStateName) then return end
   end
 
   -- Invoke pausedState on the previous state
-  local prevState = self:getCurrentState()
-  if(prevState~=nil and type(prevState.pausedState) == "function") then prevState.pausedState(self) end
+  _invokeCallback(self, self:getCurrentState(), 'pausedState')
 
   -- Do the push
   table.insert(stack, nextState)
 
-  -- Invoke pushState on the next state
-  if(type(nextState.pushedState) == "function") then nextState.pushedState(self) end
-  
+  -- Invoke pushedState & enterState on the next state
+  _invokeCallback(self, nextState, 'pushedState')
+  _invokeCallback(self, nextState, 'enterState')
+
   return nextState
 end
 
--- If a state name is given, it will attempt to remove it from the stack. If not found on the stack it will do nothing.
--- If no state name is give, this pops the top state from the stack, if any. Otherwise it does nothing.
--- Callbacks will be called when needed.
+--[[ Removes a state from the state stack
+   If a state name is given, it will attempt to remove it from the stack. If not found on the stack it will do nothing.
+   If no state name is give, this pops the top state from the stack, if any. Otherwise it does nothing.
+   Callbacks will be called when needed.
+]]
 function StatefulObject:popState(stateName)
-  assert(self.states~=nil, "Attribute 'states' not detected. check that you called instance:popState and not instance.popState, and that you invoked super.initialize(self) in the constructor.")
+  assert(_private[self].states~=nil, "Attribute 'states' not detected. check that you called instance:popState and not instance.popState, and that you invoked super.initialize(self) in the constructor.")
 
-  -- Invoke poppedState on the previous state
+  -- Invoke exitstate & poppedState on the state being popped out
   local prevState = self:getCurrentState()
-  if(prevState~=nil and type(prevState.poppedState) == "function") then prevState.poppedState(self) end
+  _invokeCallback(self, prevState, 'exitState')
+  _invokeCallback(self, prevState, 'poppedState')
 
   -- Do the pop
-  local stack = private[self].stateStack
+  local stack = _getStack(self)
   table.remove(stack, #stack)
 
   -- Invoke continuedState on the new state
   local newState = self:getCurrentState()
-  if(newState~=nil and type(newState.continuedState) == "function") then newState.continuedState(self) end
-  
+  _invokeCallback(self, newState, 'continuedState')
+
   return newState
 end
 
+--[[ Empties the state stack
+   This function will invoke all the popState, exitState callbacks on all the states as they pop out.
+]]
 function StatefulObject:popAllStates()
   local state = self:popState()
   while(state~=nil) do state = self:popState() end
 end
 
+--[[ Returns the current state (top of the stack only)
+  The current state's name can be obtained doing object:getCurrentState().name
+]]
 function StatefulObject:getCurrentState()
-  local stack = private[self].stateStack
+  local stack = _getStack(self)
   if #stack == 0 then return nil end
   return(stack[#stack])
 end
@@ -137,8 +183,8 @@ end
   Returns true if the object is in the state named 'stateName'
   If second(optional) parameter is true, this method returns true if the state is on the stack instead
 ]]
-function StatefulObject:inState(stateName, testStateStack)
-  local stack = private[self].stateStack
+function StatefulObject:isInState(stateName, testStateStack)
+  local stack = _getStack(self)
 
   if(testStateStack==true) then
     for _,state in ipairs(stack) do 
@@ -152,63 +198,57 @@ function StatefulObject:inState(stateName, testStateStack)
   return false
 end
 
-
--- Class methods
+------------------------------------
+-- CLASS METHODS
+------------------------------------
 
 --[[ Adds a new state to the "states" class member.
   superState is optional. If nil, Object will be the parent class of the new state
   returns the newly created state
 ]]
-function StatefulObject:addState(stateName, superState)
-  assert(subclassOf(StatefulObject, self), "Use class:addState instead of class.addState")
-  assert(self.states[stateName]==nil, "The class " .. self.name .. " already has a state called '" .. stateName)
+function StatefulObject.addState(theClass, stateName, superState)
+  assert(subclassOf(StatefulObject, theClass), "Use class:addState instead of class.addState")
+  assert(theClass.states[stateName]==nil, "The class " .. theClass.name .. " already has a state called '" .. stateName)
   assert(type(stateName)=="string", "stateName must be a string")
   -- states are just regular classes. If superState is nil, this uses Object as superClass
   local state = class(stateName, superState)
-  self.states[stateName] = state
+  theClass.states[stateName] = state
   return state
 end
 
--- These methods will not be overriden by the states.
-local ignoredMethods = {
-  states=1, initialize=1,
-  gotoState=1, pushState=1, popState=1, popAllStates=1, getCurrentState=1, inState=1,
-  enterState=1, exitState=1, pushedState=1, poppedState=1, pausedState=1, continuedState=1,
-  addState=1, subclass=1, includes=1
-}
-local prevSubclass = StatefulObject.subclass
---[[ creates a stateful subclass
+--[[ Redefinition of Object:subclass
   Subclasses inherit all the states of their superclases, in a special way:
   If class A has a state called Sleeping and B = A.subClass('B'), then B.states.Sleeping is a subclass of A.states.Sleeping
   returns the newly created stateful class
 ]]
-function StatefulObject:subclass(name)
-  --assert(subclassOf(StatefulObject, self), "Use class:subclass instead of class.subclass")
-  local theClass = prevSubclass(self, name) --for now, theClass is just a regular subclass
-  
+function StatefulObject.subclass(theClass, name)
+  assert(theClass==StatefulObject or subclassOf(StatefulObject, theClass), "Use class:subclass instead of class.subclass")
+
+  local theSubClass = _prevSubclass(theClass, name) --for now, theClass is just a regular subclass
+
   --the states of the subclass are subclasses of the superclass' states
-  theClass.states = {}
-  for stateName,state in pairs(self.states) do 
-    theClass:addState(stateName, state)
+  theSubClass.states = {}
+  for stateName,state in pairs(theClass.states) do 
+    theSubClass:addState(stateName, state)
   end
 
-  --make sure that the currentState is used on the method lookup function before looking on the class dict
-  local classDict = theClass.__classDict
+  --look for instance methods on the state stack before looking them up on the class' dictionary
+  local classDict = theSubClass.__classDict
   classDict.__index = function(instance, methodName)
     -- If the method isn't on the 'ignoredMethods' list, look through the stack to see if it is defined
-    if(ignoredMethods[methodName]~=1) then
-      local stack = private[instance].stateStack
+    if(_ignoredMethods[methodName]~=1) then
+      local stack = _private[instance].stateStack
       local method
       for i = #stack,1,-1 do -- reversal loop
         method = stack[i][methodName]
         if(method~=nil) then return method end
       end
     end
-    --if ignored or not found, look on the class itself
+    --if ignored or not found, look on the class method
     return classDict[methodName]
   end
 
-  return theClass
+  return theSubClass
 end
 
 
@@ -217,17 +257,17 @@ end
      then each member of that module.states is included on the StatefulObject class.
      If module.states has a state that doesn't exist on StatefulObject, a new state will be created.
 ]]
-function StatefulObject:includes(module, ...)
-  assert(subclassOf(StatefulObject, self), "Use class:includes instead of class.includes")
+function StatefulObject.includes(theClass, module, ...)
+  assert(subclassOf(StatefulObject, theClass), "Use class:includes instead of class.includes")
   for methodName,method in pairs(module) do
     if methodName ~="included" and methodName ~= "states" then
-      self[methodName] = method
+      theClass[methodName] = method
     end
   end
-  if type(module.included)=="function" then module.included(self, ...) end
+  if type(module.included)=="function" then module.included(theClass, ...) end
   if type(module.states)=="table" then
     for stateName,moduleState in pairs(module.states) do 
-      local state = self.states[stateName]
+      local state = theClass.states[stateName]
       if(state==nil) then state = theClass:addState(stateName) end
       state:includes(moduleState, ...)
     end
