c68891cf576a998c9fd863c61f24be8a5d9e514e
Bart van Strien <bart@bartbes.ath.cx>
2011-10-23 13:59:59 +0200

    Make implementation standards-compliant

--- middleclass-commons.lua
+++ middleclass-commons.lua
@@ -1,13 +1,14 @@
-common = type(common) == "table" and common or {}
+if common_class ~= false then
+	common = type(common) == "table" and common or {}
 
-function common.class(name, class, superclass)
-	superclass = superclass or Object
-	local c = superclass:subclass(name)
-	for i, v in pairs(class) do
-		c[i] = v
+	function common.class(name, class, superclass)
+		local c = (superclass or Object):subclass(name)
+		for i, v in pairs(class) do
+			c[i] = v
+		end
+		if class.init then
+			c.initialize = class.init
+		end
+		return c
 	end
-	if class.init then
-		c.initialize = class.init
-	end
-	return c
 end
