583a3d29e17a87f479163afb68bc98b7278aabc6
kikito <kikito@gmail.com>
2013-09-17 01:04:54 +0200

    fixed subclasOf_spec

--- spec/subclassOf_spec.lua
+++ spec/subclassOf_spec.lua
@@ -1,7 +1,7 @@
 local class = require 'middleclass'
 local Object = class.Object
 
-context('subclassOf', function()
+context('isSubclassOf', function()
 
   context('nils, integers, etc', function()
     local primitives = {nil, 1, 'hello', {}, function() end}
@@ -10,23 +10,23 @@ context('subclassOf', function()
       local theType = type(primitive)
       context('A ' .. theType, function()
 
-        local f1 = function() return subclassOf(Object, primitive) end
-        local f2 = function() return subclassOf(primitive, o) end
-        local f3 = function() return subclassOf(primitive, primitive) end
+        local f1 = function() return Object.isSubclassOf(Object, primitive) end
+        local f2 = function() return Object.isSubclassOf(primitive, o) end
+        local f3 = function() return Object.isSubclassOf(primitive, primitive) end
 
         context('does not throw errors', function()
-          test('subclassOf(Object, '.. theType ..')', function()
+          test('isSubclassOf(Object, '.. theType ..')', function()
             assert_not_error(f1)
           end)
-          test('subclassOf(' .. theType .. ', Object:new())', function()
+          test('isSubclassOf(' .. theType .. ', Object:new())', function()
             assert_not_error(f2)
           end)
-          test('subclassOf(' .. theType .. ',' .. theType ..')', function()
+          test('isSubclassOf(' .. theType .. ',' .. theType ..')', function()
             assert_not_error(f3)
           end)
         end)
 
-        test('makes subclassOf return false', function()
+        test('makes isSubclassOf return false', function()
           assert_false(f1())
           assert_false(f2())
           assert_false(f3())
@@ -43,31 +43,31 @@ context('subclassOf', function()
     local Class3 = class('Class3', Class2)
     local UnrelatedClass = class('Unrelated')
 
-    test('is subclassOf(Object)', function()
-      assert_true(subclassOf(Object, Class1))
-      assert_true(subclassOf(Object, Class2))
-      assert_true(subclassOf(Object, Class3))
+    test('isSubclassOf(Object)', function()
+      assert_true(Class1:isSubclassOf(Object))
+      assert_true(Class2:isSubclassOf(Object))
+      assert_true(Class3:isSubclassOf(Object))
     end)
 
     test('is subclassOf its direct superclass', function()
-      assert_true(subclassOf(Class1, Class2))
-      assert_true(subclassOf(Class2, Class3))
+      assert_true(Class2:isSubclassOf(Class1))
+      assert_true(Class3:isSubclassOf(Class2))
     end)
 
     test('is subclassOf its ancestors', function()
-      assert_true(subclassOf(Class1, Class3))
+      assert_true(Class3:isSubclassOf(Class1))
     end)
 
     test('is a subclassOf its class\' subclasses', function()
-      assert_false(subclassOf(Class2, Class1))
-      assert_false(subclassOf(Class3, Class1))
-      assert_false(subclassOf(Class3, Class2))
+      assert_true(Class2:isSubclassOf(Class1))
+      assert_true(Class3:isSubclassOf(Class1))
+      assert_true(Class3:isSubclassOf(Class2))
     end)
 
     test('is not a subclassOf an unrelated class', function()
-      assert_false(subclassOf(UnrelatedClass, Class1))
-      assert_false(subclassOf(UnrelatedClass, Class2))
-      assert_false(subclassOf(UnrelatedClass, Class3))
+      assert_false(Class1:isSubclassOf(UnrelatedClass))
+      assert_false(Class2:isSubclassOf(UnrelatedClass))
+      assert_false(Class3:isSubclassOf(UnrelatedClass))
     end)
 
   end)
