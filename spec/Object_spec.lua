d15cc2519424548132eef2c5ca126e7f4b81ec9a
kikito <kikito@gmail.com>
2013-09-18 23:42:16 +0200

    added subclassOf, instanceOf and includes specs to object

--- spec/Object_spec.lua
+++ spec/Object_spec.lua
@@ -66,7 +66,7 @@ describe('Object', function()
       function SubClass:initialize() self.mark=true end
     end)
 
-   describe('allocate', function()
+    describe('allocate', function()
 
       it('allocates instances properly', function()
         local instance = SubClass:allocate()
@@ -118,8 +118,224 @@ describe('Object', function()
 
     end)
 
+    describe('isInstanceOf', function()
+
+      describe('nils, integers, strings, tables, and functions', function()
+        local o = Object:new()
+        local primitives = {nil, 1, 'hello', {}, function() end}
+
+        for _,primitive in pairs(primitives) do
+          local theType = type(primitive)
+          describe('A ' .. theType, function()
+
+            local f1 = function() return Object.isInstanceOf(primitive, Object) end
+            local f2 = function() return Object.isInstanceOf(primitive, o) end
+            local f3 = function() return Object.isInstanceOf(primitive, primitive) end
+
+            describe('does not throw errors', function()
+              it('instanceOf(Object, '.. theType ..')', function()
+                assert.not_error(f1)
+              end)
+              it('instanceOf(' .. theType .. ', Object:new())', function()
+                assert.not_error(f2)
+              end)
+              it('instanceOf(' .. theType .. ',' .. theType ..')', function()
+                assert.not_error(f3)
+              end)
+            end)
+
+            it('makes instanceOf return false', function()
+              assert.is_false(f1())
+              assert.is_false(f2())
+              assert.is_false(f3())
+            end)
+
+          end)
+        end
+
+      end)
+
+      describe('An instance', function()
+        local Class1 = class('Class1')
+        local Class2 = class('Class2', Class1)
+        local Class3 = class('Class3', Class2)
+        local UnrelatedClass = class('Unrelated')
+
+        local o1, o2, o3 = Class1:new(), Class2:new(), Class3:new()
+
+        it('isInstanceOf(Object)', function()
+          assert.is_true(o1:isInstanceOf(Object))
+          assert.is_true(o2:isInstanceOf(Object))
+          assert.is_true(o3:isInstanceOf(Object))
+        end)
+
+        it('isInstanceOf its class', function()
+          assert.is_true(o1:isInstanceOf(Class1))
+          assert.is_true(o2:isInstanceOf(Class2))
+          assert.is_true(o3:isInstanceOf(Class3))
+        end)
+
+        it('is instanceOf its class\' superclasses', function()
+          assert.is_true(o2:isInstanceOf(Class1))
+          assert.is_true(o3:isInstanceOf(Class1))
+          assert.is_true(o3:isInstanceOf(Class2))
+        end)
+
+        it('is not instanceOf its class\' subclasses', function()
+          assert.is_false(o1:isInstanceOf(Class2))
+          assert.is_false(o1:isInstanceOf(Class3))
+          assert.is_false(o2:isInstanceOf(Class3))
+        end)
+
+        it('is not instanceOf an unrelated class', function()
+          assert.is_false(o1:isInstanceOf(UnrelatedClass))
+          assert.is_false(o2:isInstanceOf(UnrelatedClass))
+          assert.is_false(o3:isInstanceOf(UnrelatedClass))
+        end)
+
+      end)
+
+    end)
 
   end)
+
+  describe('isSubclassOf', function()
+
+    describe('nils, integers, etc', function()
+      local primitives = {nil, 1, 'hello', {}, function() end}
+
+      for _,primitive in pairs(primitives) do
+        local theType = type(primitive)
+        describe('A ' .. theType, function()
+
+          local f1 = function() return Object.isSubclassOf(Object, primitive) end
+          local f2 = function() return Object.isSubclassOf(primitive, o) end
+          local f3 = function() return Object.isSubclassOf(primitive, primitive) end
+
+          describe('does not throw errors', function()
+            it('isSubclassOf(Object, '.. theType ..')', function()
+              assert.not_error(f1)
+            end)
+            it('isSubclassOf(' .. theType .. ', Object:new())', function()
+              assert.not_error(f2)
+            end)
+            it('isSubclassOf(' .. theType .. ',' .. theType ..')', function()
+              assert.not_error(f3)
+            end)
+          end)
+
+          it('makes isSubclassOf return false', function()
+            assert.is_false(f1())
+            assert.is_false(f2())
+            assert.is_false(f3())
+          end)
+
+        end)
+      end
+
+    end)
+
+    describe('Any class (except Object)', function()
+      local Class1 = class('Class1')
+      local Class2 = class('Class2', Class1)
+      local Class3 = class('Class3', Class2)
+      local UnrelatedClass = class('Unrelated')
+
+      it('isSubclassOf(Object)', function()
+        assert.is_true(Class1:isSubclassOf(Object))
+        assert.is_true(Class2:isSubclassOf(Object))
+        assert.is_true(Class3:isSubclassOf(Object))
+      end)
+
+      it('is subclassOf its direct superclass', function()
+        assert.is_true(Class2:isSubclassOf(Class1))
+        assert.is_true(Class3:isSubclassOf(Class2))
+      end)
+
+      it('is subclassOf its ancestors', function()
+        assert.is_true(Class3:isSubclassOf(Class1))
+      end)
+
+      it('is a subclassOf its class\' subclasses', function()
+        assert.is_true(Class2:isSubclassOf(Class1))
+        assert.is_true(Class3:isSubclassOf(Class1))
+        assert.is_true(Class3:isSubclassOf(Class2))
+      end)
+
+      it('is not a subclassOf an unrelated class', function()
+        assert.is_false(Class1:isSubclassOf(UnrelatedClass))
+        assert.is_false(Class2:isSubclassOf(UnrelatedClass))
+        assert.is_false(Class3:isSubclassOf(UnrelatedClass))
+      end)
+
+    end)
+  end)
+
+  describe('includes', function()
+
+    describe('nils, numbers, etc', function()
+      local o = Object:new()
+      local primitives = {nil, 1, 'hello', {}, function() end}
+
+      for _,primitive in pairs(primitives) do
+        local theType = type(primitive)
+        describe('A ' .. theType, function()
+
+          local f1 = function() return Object.includes(Object, primitive) end
+          local f2 = function() return Object.includes(primitive, o) end
+          local f3 = function() return Object.includes(primitive, primitive) end
+
+
+          describe('don\'t throw errors', function()
+            it('includes(Object, '.. theType ..')', function()
+              assert.not_error(f1)
+            end)
+            it('includes(' .. theType .. ', Object:new())', function()
+              assert.not_error(f2)
+            end)
+            it('includes(' .. theType .. ',' .. theType ..')', function()
+              assert.not_error(f3)
+            end)
+          end)
+
+          it('make includes return false', function()
+            assert.is_false(f1())
+            assert.is_false(f2())
+            assert.is_false(f3())
+          end)
+
+        end)
+      end -- for
+
+    end)
+
+    describe('A class', function()
+
+      local Class1 = class('Class1')
+      local Class2 = class('Class2', Class1)
+      local Class3 = class('Class3', Class2)
+      local UnrelatedClass = class('Unrelated')
+
+      local hasFoo = { foo=function() return 'foo' end }
+      Class1:include(hasFoo)
+
+      it('returns true if it includes a mixin', function()
+        assert.is_true(Class1:includes(hasFoo))
+      end)
+
+      it('returns true if its superclass includes a mixin', function()
+        assert.is_true(Class2:includes(hasFoo))
+        assert.is_true(Class3:includes(hasFoo))
+      end)
+
+      it('returns false otherwise', function()
+        assert.is_false(UnrelatedClass:includes(hasFoo))
+      end)
+
+    end)
+
+  end)
+
 
 end)
 
