d97a1bfbd4a31b4086d86afc895aa6b51217cbf7
kikito <kikito@gmail.com>
2015-10-31 15:56:17 +0100

    do not stub the __gc metamethod

--- middleclass.lua
+++ middleclass.lua
@@ -98,7 +98,7 @@ end
 local Object = _createClass("Object", nil)
 
 Object.static.__metamethods = { '__add', '__band', '__bor', '__bxor', '__bnot', '__call', '__concat', '__div', '__eq',
-                                '__gc', '__ipairs', '__idiv', '__le', '__len', '__lt', '__metatable', '__mod', '__mode',
+                                '__ipairs', '__idiv', '__le', '__len', '__lt', '__metatable', '__mod', '__mode',
                                 '__mul', '__pairs', '__pow', '__sh1', '__shr', '__sub', '__tostring', '__unm' }
 
 function Object.static:allocate()
